796770e6b55d02bdaff81dd4ede612bb14bed6ef
alexwilkinson <alex.wilkinson@me.com>
2014-05-21 20:03:04 -0400

    wrote passing controller spec for redirect

--- spec/controllers/searches_controller_spec.rb
+++ spec/controllers/searches_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe SearchesController do
-
+  describe "GET #show" do
+    it 'should redirect to dashboard' do
+      get :show, address: 'Yale University', city: 'New Haven', state: 'CT'
+      expect(response).to be_a_redirect
+    end
+  end
 end
---
+++ spec/features/searching_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+feature 'Searching' do
+
+  xscenario 'entering a valid address' do
+    
+  end
+
+end
